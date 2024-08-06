b23f539d2cdfde67aa980a24723f0014676a73f7
jasoncodes-ca <github@jasoncodes.ca>
2024-08-06 00:23:24 -0700

    Change get_line() to receive any length line

--- chapter_1/exercise_16.c
+++ chapter_1/exercise_16.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
-
+#define MAXLINE 1000
 int get_line(char line[], int cap);
-void expand_array(char str[], int cap);
 void copy(char to[], char from[]);
 
 /*print longest input line */
 int main() {
   int len;    /* current line length */
   int max;    /* max length seen so far */
-  int cap = 8; /* capacity of the char arrays. */ 
-  char line[cap]; /* current input line */
-  char longest[cap]; /* longest line saved here */
+  char line[MAXLINE]; /* current input line */
+  char longest[MAXLINE]; /* longest line saved here */
 
-  while( (len = get_line(line, cap)) > 0 ) {
+  while( (len = get_line(line, MAXLINE)) > 0 ) {
     if (len > max) {
       max = len;
       copy(longest, line);
@@ -27,42 +25,26 @@ int main() {
 
 
 /* get_line: read a line into string_array, return its length */
-int get_line(char string_array[], int cap) {
+int get_line(char string_array[], int lim) {
   int c, i;
-  
-  while ( (c = getchar()) != EOF && c != '\n') {
-
-    if (i >= cap) {
-      expand_array(string_array, cap);
-    }
-
+  for (i = 0; i < lim - 1 && (c=getchar() != EOF) && c != '\n'; i++) {
     string_array[i] = c;
   }
 
   if (c == '\n') {
-    string_array[i] = c;
-    ++i;
+    string_array[i++] = '\0';
   }
 
-  string_array[i] = '\0';
-  return i;
+  while (c != '\n' || c != EOF) {
+    i++;
+  }
+  return i; 
 }
-
-/* expand_array: double the length of the passed in array, return new cap */
-int expand_array(char arr[], int cap) {
-  int new_cap = cap * 2;
-  char expanded[new_cap];
-  copy(expanded, arr);
-  arr = expanded;
-  return new_cap;
-}
-
 
 /* copy: copy 'from' into 'to'; assume to is big enough */
 void copy(char to[], char from[]) {
   int i;
 
-  i = 0;
   while ( (to[i] = from[i]) != '\0'){
     ++i;
   }
